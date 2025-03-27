505f0d8d3848ba20da8b7cef81cf7fc606188d49
Nandhu <nandhukrishna1217@gmail.com>
2025-03-27 22:51:40 +0530

    Fixed low res video call

--- lib/screens/videocall.dart
+++ lib/screens/videocall.dart
@@ -152,15 +152,15 @@ class VideoCallWidgetState extends State<VideoCallWidget> {
           const SizedBox(height: 10),
           // Local video stream
           SizedBox(
-            width: 150,
-            height: 100,
+            width: 360,
+            height: 270,
             child: RTCVideoView(_localRenderer),
           ),
           const SizedBox(height: 10),
           // Remote video stream
           SizedBox(
-            width: 150,
-            height: 100,
+            width: 360,
+            height: 270,
             child: RTCVideoView(_remoteRenderer),
           ),
           const SizedBox(height: 10),
